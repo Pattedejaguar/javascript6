0d2debccbb7311ee05f8d148378f0e8558018f22
Yannick L <lef.yannick@gmail.com>
2019-04-09 15:55:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function ex6(){
+// {
+//   var number1= document.getElementById('firstNumber').value
+//   var number2= document.getElementById('secondNumber').value
+//   alert(number1 % number2)
+// }
+
+
+alert("Reste de la division : " + firstNumber.value % secondNumber.value);
+}
